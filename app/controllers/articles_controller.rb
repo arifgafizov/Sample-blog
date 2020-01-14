ee3e79ea77bffbf8177719476d486c88dfa5928d
Arif Gafizov <agafizov@gmail.com>
2020-01-14 10:59:48 +0300

    Update articles#create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     if @article.valid? # условие если модель валидна
       @article.save
+      redirect_to @article # redirect_to получает сущность @article и делает автоматом редирект (перенаправление)
     else
       render action: 'new' # возвращаем результат ключа action 'new' т.е. представление new.html.erb, если не прошла валидация
     end
