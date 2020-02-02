2630013ca53099ec0bee07a3d4e9f11c712d15c8
Arif <agafizov@gmail.com>
2020-02-02 13:56:01 +0300

    Add username column to comments table

--- db/migrate/20200202104656_add_username_comment.rb
+++ db/migrate/20200202104656_add_username_comment.rb
@@ -1,4 +1,6 @@
 class AddUsernameComment < ActiveRecord::Migration[6.0]
   def change
+      add_column :comments, :username, :string
+      add_index :comments, :username, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_29_083950) do
+ActiveRecord::Schema.define(version: 2020_02_02_104656) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,7 +25,9 @@ ActiveRecord::Schema.define(version: 2020_01_29_083950) do
     t.integer "article_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
     t.index ["article_id"], name: "index_comments_on_article_id"
+    t.index ["username"], name: "index_comments_on_username", unique: true
   end
 
   create_table "contacts", force: :cascade do |t|
