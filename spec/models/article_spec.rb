48b6f8469b45e0b61dbdd28e996714aa104a5fbf
Arif <agafizov@gmail.com>
2020-02-10 21:21:23 +0300

    Update article spec to include validations

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,6 +1,13 @@
 require "spec_helper"
 require 'rails_helper'
 
+# разбивка модели теста на две логические части "validations" и "associations"
 describe Article do
-  it { should have_many :comments } # проверка наличия связи one-to-many статьи к коментариям в сущности Article
+  describe "validations" do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :text }
+  end
+  describe "associations" do
+    it { should have_many :comments } # проверка наличия связи one-to-many статьи к коментариям в сущности Article
+  end
 end
